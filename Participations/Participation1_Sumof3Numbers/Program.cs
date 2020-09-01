d51dc3348e128816a6493715ddeded05bdcb715d
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2020-09-01 11:49:40 -0500

    Create a new repository with starter solutions+projects

---
+++ Participations/Participation1_Sumof3Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Participation1_Sumof3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
