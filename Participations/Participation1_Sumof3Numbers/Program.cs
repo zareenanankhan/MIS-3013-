88db41ccb2eaab8fc6357d8f3a09e8d08473c407
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2020-09-03 22:59:39 -0500

    SumOfThree COMMIT

--- Participations/Participation1_Sumof3Numbers/Program.cs
+++ Participations/Participation1_Sumof3Numbers/Program.cs
@@ -8,7 +8,28 @@ namespace Participation1_Sumof3Numbers
         const double NUM = 7.777;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter first number");
+            string InputNumOne = Console.ReadLine();
+            double NumOne = Convert.ToDouble(InputNumOne);
+            // stores user input for the first value
+
+            Console.WriteLine("Enter second number");
+            string InputNumTwo = Console.ReadLine();
+            double NumTwo = Convert.ToDouble(InputNumTwo);
+            // stores user input for second value 
+
+            Console.WriteLine("Enter third number");
+            string InputNumThree = Console.ReadLine();
+            double NumThree = Convert.ToDouble(InputNumThree);
+            // stores user input for third value
+
+            double SumOfThree = NumOne + NumTwo + NumThree;
+            Console.WriteLine("Sum of three inputs is: " + SumOfThree);
+            // adds and prints the sum
+
+            double SecondValue = SumOfThree * NUM;
+            Console.WriteLine("The second value is: " + SecondValue);
+            // multiplies the summed value with the constant double and prints it
         }
     }
 }
